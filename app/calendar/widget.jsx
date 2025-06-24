446a56d30604493bc72d091c9ddf4b1c84ad6319
Panayiotis <1040249+panayiotis@users.noreply.github.com>
2025-06-23 21:48:54 -0400

    Minor change to calendar

--- app/calendar/widget.jsx
+++ app/calendar/widget.jsx
@@ -12,11 +12,10 @@ const Widget = () => {
       <Calendar />
       <div className={`flex-1 flex flex-col pl-3 ${border}`}>
         <p className='text-sm opacity-100 mt-1'>
-          {`🚙 in ${1+Math.floor((new Date('2025-06-18') - new Date()) / (1000 * 60 * 60 * 24))} days`}<br />
-          {`⛱️ in ${1+Math.floor((new Date('2025-06-19') - new Date()) / (1000 * 60 * 60 * 24))} days`}
+          {`⛱️ in ${1 + Math.floor((new Date('2025-07-04') - new Date()) / (1000 * 60 * 60 * 24))} days`}
         </p>
         <p className='text-sm opacity-80 mt-1'>
-          {`${1+Math.floor((new Date() - new Date('2025-05-23')) / (1000 * 60 * 60 * 24))} days in 🇺🇸`}
+          {`${Math.floor((new Date() - new Date('2025-05-23')) / (1000 * 60 * 60 * 24))} days in 🇺🇸`}
         </p>
       </div>
     </div>
